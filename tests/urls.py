5bb70514eb6457fed9f793a7fac6e8fed8e9c15b
jrog612 <wnrhd114@gmail.com>
2021-06-28 15:40:48 +0900

    remove unused view import code in test url

--- tests/urls.py
+++ tests/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 
-from tests.views.test_extra import GetterResponseTestAPIView
-from tests.views.test_filtering import FilteringTestAPI
 from tests.test_exceptions import TestExceptionAPI
 
 urlpatterns = [
