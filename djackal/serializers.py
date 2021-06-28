da48dc4ae4ef98786d4d2af1ab897f95b260f498
jrog612 <wnrhd114@gmail.com>
2021-06-28 15:43:04 +0900

    BaseModelSerializer add has_many and extra_standard_fields for build_fields

--- djackal/serializers.py
+++ djackal/serializers.py
@@ -2,6 +2,19 @@ from rest_framework import serializers
 
 
 class BaseModelSerializer(serializers.ModelSerializer):
+    extra_standard_fields = ()
+
+    def __new__(cls, *args, **kwargs):
+        if kwargs.get('many', False) is True:
+            context = kwargs.get('context', {})
+            context.update(has_many=kwargs.get('many', True))
+            kwargs.update(context=context)
+        return super().__new__(cls, *args, **kwargs)
+
+    @property
+    def has_many(self):
+        return self.context.get('has_many', False)
+
     def get_fields(self):
         fields = super().get_fields()
 
@@ -9,7 +22,6 @@ class BaseModelSerializer(serializers.ModelSerializer):
             if isinstance(fields[field_name], serializers.ListSerializer):
                 if isinstance(fields[field_name].child, BaseModelSerializer):
                     fields[field_name].child._context = self._context
-
             elif isinstance(fields[field_name], BaseModelSerializer):
                 fields[field_name]._context = self._context
 
@@ -23,3 +35,9 @@ class BaseModelSerializer(serializers.ModelSerializer):
         if request is None:
             return self.context.get('user') or self.context.get('current_user')
         return request.user
+
+    def build_field(self, field_name, info, model_class, *args, **kwargs):
+        model_field = model_class._meta.get_field(field_name)
+        if model_field.__class__ in self.extra_standard_fields:
+            return self.build_standard_field(field_name, model_field)
+        return super().build_field(field_name, info, model_class, *args, **kwargs)
