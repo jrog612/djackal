a4ae1fc56f5cf0ba7bb0b9d90ecd8e0afbb59274
jrog612 <wnrhd114@gmail.com>
2022-04-20 20:20:30 +0900

    Use APIRequestFactory in text_exceptions

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,6 +1,8 @@
 import http
 import random
 
+from rest_framework.test import APIRequestFactory
+
 from djackal.erra import Erra
 from djackal.exceptions import DjackalAPIException, NotFound, Forbidden, BadRequest, InternalServer, ErraException, \
     Unauthorized, NotAllowed
@@ -18,12 +20,14 @@ TEST_FORBIDDEN_CODE = 'TEST_FORBIDDEN'
 TEST_NOT_ALLOWED_CODE = 'TEST_NOT_ALLOWED'
 TEST_INTERNAL_SERVER_CODE = 'TEST_INTERNAL_SERVER'
 
+factory = APIRequestFactory()
+
 
 class TestException(DjackalAPIException):
     default_status_code = 501
 
 
-class TestExceptionAPI(DjackalAPIView):
+class ExceptionAPI(DjackalAPIView):
     def post(self, request):
         kind = request.data['kind']
         status_code = request.data.get('status_code')
@@ -48,21 +52,28 @@ class TestExceptionAPI(DjackalAPIView):
 
 
 class ExceptionTest(DjackalAPITestCase):
+    def setUp(self):
+        self.view = ExceptionAPI.as_view()
+
     def test_exception_handle(self):
-        response = self.client.get('/exception')
+        request = factory.get('/')
+        response = self.view(request)
         self.assertStatusCode(501, response)
 
     def _check_sub_erra_exception(self, kind, code, status_code):
-        response = self.client.post('/exception', {'kind': kind})
+        request = factory.post('/', {'kind': kind})
+        response = self.view(request)
         self.assertStatusCode(status_code, response)
-        result = response.json()
+        result = response.data
         self.assertEqual(result['test'], True)
         self.assertEqual(result['code'], code)
 
     def test_exception_response(self):
-        response = self.client.post('/exception', {'kind': 'NotFound'})
+        request = factory.post('/', {'kind': 'NotFound'})
+        response = self.view(request)
         self.assertStatusCode(404, response)
-        result = response.json()
+
+        result = response.data
         self.assertEqual(result['test'], True)
         self.assertEqual(result['code'], 'NOT_FOUND')
 
@@ -71,16 +82,20 @@ class ExceptionTest(DjackalAPITestCase):
         self._check_sub_erra_exception('Forbidden', TEST_FORBIDDEN_CODE, 403)
         self._check_sub_erra_exception('NotAllowed', TEST_NOT_ALLOWED_CODE, 405)
 
-        response = self.client.post('/exception', {'kind': 'Erra'})
+        request = factory.post('/', {'kind': 'Erra'})
+        response = self.view(request)
         self.assertStatusCode(500, response)
-        result = response.json()
+        result = response.data
         self.assertEqual(result['test'], True)
         self.assertEqual(result['code'], TestErra.TEST_ERRA.code)
         self.assertEqual(result['message'], TestErra.TEST_ERRA.message)
 
     def test_exception_status_code(self):
-        response = self.client.post('/exception', {'kind': 'Erra'})
+        request = factory.post('/', {'kind': 'Erra'})
+        response = self.view(request)
         self.assertStatusCode(500, response)
         code = random.choice(list(http.HTTPStatus))
-        response = self.client.post('/exception', {'kind': 'Erra', 'status_code': code.value})
+
+        request = factory.post('/', {'kind': 'Erra', 'status_code': code.value})
+        response = self.view(request)
         self.assertStatusCode(code.value, response)
